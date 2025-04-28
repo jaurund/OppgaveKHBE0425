1c5f95940b9219382546183fc312c9703041ad59
Jørund <sjv042025@gmail.com>
2025-04-28 14:19:13 +0000

    lagrer for å kunne hente fra annen PC

--- Program.cs
+++ Program.cs
@@ -1,54 +1,118 @@
 ﻿namespace OppgaveKHBE0424;
 
+
+
 class Program
 {
     static void Main(string[] args)
     {
-        // initalize a new object, from a class
-        Person person = new Person();
-        person.Name = "Marius";
-        person.Age = 39;
+       
         
-        Console.WriteLine(person.Name);
-        Console.WriteLine(person.Age);
-
-        string[] shoppingList = {"Milk", "Soda", "Dinner", "Candy", "Fish", "Salmon"}; // fixed size: new string[6]
-        List<string> programmeringLanguages = new List<string>() {
-            "C#", "Java", "Python", "JavaScript", "C", "C++", "Rust", "Go"
-        };
-
-        List<int> ints = [1,2,3,4];
-
-        for(int i = 0; i < shoppingList.Length; i++) 
-        {
-            Console.WriteLine(shoppingList[i]);
-        }
-
-        foreach(var programming in programmeringLanguages) 
-        {
-            Console.WriteLine(programming);
-        }
 
         /*
             Oppgave 1:
                 - Lag noen enkle variabler av forksjellige primitive datatyper
                 - Forsøk å skrive ut verdiene som er tildelt variablene ved å bruke Console.WriteLine(); metoden.
         */
+        // int alder = 30;
+        // Her vil jeg heller regne ut alderen ut i fra fødselsdato, i stedet for å bare skrive det inn
+        DateTime fødselsdato = new DateTime(1995, 1, 1);
+        DateTime iDag = DateTime.Today;
+        int alder = iDag.Year - fødselsdato.Year;
+        if (fødselsdato.Date > iDag.AddYears(-alder))
+        {
+            alder--;
+        }
 
+        double høyde = 1.86;
+        bool erStudent = false;
+        char kjønn = 'M';
+        string navn = "William";
+
+
+        // string fødselsdato = "01.01.1995";
+        // ChatGPT forteller at primitive datatyper ikke er så bra for å printe ut fødselsdatoer, og anbefalte heller funksjonen DateTime. Det er det jeg har gjort her, og bygget videre oppgaven min på.
+
+
+        Console.WriteLine($"Navn: {navn}");
+        Console.WriteLine($"Alder: {alder}");
+        Console.WriteLine($"Høyde: {høyde} meter");
+        Console.WriteLine($"Er student: {erStudent}");
+        Console.WriteLine($"Kjønn: {kjønn}");
+        Console.WriteLine($"Fødselsdato: {fødselsdato.ToShortDateString()}");
+        Console.WriteLine($"Alder: {alder}");
+        // Console.WriteLine($"Fødselsdato: {fødselsdato}");
 
         /*
             Oppgave 2:
                 - Forsøk å lage et array av forksjellige primitive datatyper.
                 - Forsøk å skrive ut verdiene som befinner seg i arrayet, ved å bruke en for-loop, eller en foreach-loop.
         */
+        object [] personData = new object []
+        {
+            "William",
+            30,
+            1.86,
+            true,
+            'M',
+            new DateTime(1995, 1, 1)
+        };
+
+        Console.WriteLine("\nVerdier i arrayet:");
+        foreach (var data in personData)
+        {
+            Console.WriteLine($"Verdi: {data}, Type: {data.GetType()}");
+        }
+        
+
+
 
         /*
             Oppgave 3:
                 - Forsøk å tildele variabler til en generiske Liste: eksempel: List<int> integers = new List<int>();
                 - Forsøk å skrive ut verdiene i listen, ved å bruke en for-loop eller en foreach-loop.
         */
+        List<Person> personer = new List<Person>();
 
+        personer.Add(new Person("William", 30, 1.86, false, 'M', new DateTime(1995, 1, 1)));
+        personer.Add(new Person("Tuva", 29,  true, 'F', new DateTime(1995, 8, 6)));
+        personer.Add(new Person("Gary", 30,  false, 'M', new DateTime(1995, 4, 17)));
+
+        Console.WriteLine("\nPersoner i listen:");
+        foreach (var person in personer)
+
+        {
+            Console.WriteLine($"Navn: {person.Navn}, Alder: {person.Alder}, Høyde: {person.Høyde} meter, Er student: {person.ErStudent}, Kjønn: {person.Kjønn}, Fødselsdato: {person.Fødselsdato.ToShortDateString()}");
+        }
     }
+}
+
+
+public class Person
+
+{
+    public string Navn { get; set; }
+    public int Alder { get; set; }
+    public double Høyde { get; set; }
+    public bool ErStudent { get; set; }
+    public char Kjønn { get; set; }
+    public DateTime Fødselsdato { get; set; }
+
+    public Person(string navn, int alder, double høyde, bool erStudent, char kjønn, DateTime fødselsdato)
+    {
+        Navn = navn;
+        Alder = alder;
+        Høyde = høyde;
+        ErStudent = erStudent;
+        Kjønn = kjønn;
+        Fødselsdato = fødselsdato;
+    }
+
+       
+
+        
+
+    
 
     	public static double Add(double a, double b) 
 		{
