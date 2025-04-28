184d6553e29c0f8f99c5da74e4e43127a996fe7f
JorgenMJobloop <jorgen@jobloop.no>
2025-04-28 06:56:11 +0000

    inital commit

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿namespace OppgaveKHBE0424;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        /*
+            Oppgave 1:
+                - Lag noen enkle variabler av forksjellige primitive datatyper
+                - Forsøk å skrive ut verdiene som er tildelt variablene ved å bruke Console.WriteLine(); metoden.
+        */
+
+
+        /*
+            Oppgave 2:
+                - Forsøk å lage et array av forksjellige primitive datatyper.
+                - Forsøk å skrive ut verdiene som befinner seg i arrayet, ved å bruke en for-loop, eller en foreach-loop.
+        */
+
+        /*
+            Oppgave 3:
+                - Forsøk å tildele variabler til en generiske Liste: eksempel: List<int> integers = new List<int>();
+                - Forsøk å skrive ut verdiene i listen, ved å bruke en for-loop eller en foreach-loop.
+        */
+        Console.WriteLine("Hello, World!");
+    }
+}
